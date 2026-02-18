a6cdfcc6d4d586e32e40bc2f8a354b0bea2877d4
tomo-local <tomohiro.togashi@gmail.com>
2026-02-19 00:18:53 +0900

    feat: deletedのフラグを追加

--- app/kv/entry.go
+++ app/kv/entry.go
@@ -8,24 +8,34 @@ import (
 type Entry struct {
 	key []byte
 	val []byte
+	deleted bool
 }
 
 func (ent *Entry) Encode() []byte {
-	// 初期のデータ構造を定義 (key)4byte+（val)4byte+keyサイズ+valサイズ
-	data := make([]byte, 4+4+len(ent.key)+len(ent.val))
+	// 初期のデータ構造を定義 (key)4byte+（val)4byte+(deleted)1byte+keyサイズ+valサイズ
+	data := make([]byte, 4+4+1+len(ent.key)+len(ent.val))
 	// keyの文字数
   binary.LittleEndian.PutUint32(data[0:4], uint32(len(ent.key)))
-	// valの文字数
-  binary.LittleEndian.PutUint32(data[4:8], uint32(len(ent.val)))
-	//
-	copy(data[8:], ent.key)
-	copy(data[8+len(ent.key):], ent.val)
+	// keyの値
+	copy(data[9:], ent.key)
+	// deletedの値
+	// 削除されるので、valの値は設定しない
+	if ent.deleted {
+		data[8] = 1
+	} else {
+		// deletedがfalseなので、値が設定される
+		data[8] = 0
+		// valの文字数
+		binary.LittleEndian.PutUint32(data[4:8], uint32(len(ent.val)))
+		copy(data[9+len(ent.key):], ent.val)
+	}
+
 	return data
 }
 
 func (ent *Entry) Decode(r io.Reader) error {
 	// 最初のkeyとvalのサイズを格納する変数を定義
-	var header [8]byte
+	var header [9]byte
 	// [8]bytesまで、データを読み込んでねということ
 	if _, err := io.ReadFull(r, header[:]); err != nil {
 		return err
@@ -35,6 +45,8 @@ func (ent *Entry) Decode(r io.Reader) error {
 	keyLen := int(binary.LittleEndian.Uint32(header[0:4]))
 	// valの文字数
 	valLen := int(binary.LittleEndian.Uint32(header[4:8]))
+	// deleted
+	deleted := header[8]
 
 	data := make([]byte, keyLen+valLen)
 	if _, err := io.ReadFull(r, data); err != nil {
@@ -43,7 +55,13 @@ func (ent *Entry) Decode(r io.Reader) error {
 
 	// 最初からkeyLenの長さまで取得
 	ent.key = data[:keyLen]
-	// keyLenの長さから最後まで取得
-	ent.val = data[keyLen:]
+	if deleted != 0 {
+		ent.deleted = true
+	} else {
+		ent.deleted = false
+		// keyLenの長さから最後まで取得
+		ent.val = data[keyLen:]
+	}
+
 	return nil
 }
--- app/kv/entry_test.go
+++ app/kv/entry_test.go
@@ -9,12 +9,24 @@ import (
 
 func TestEntryEncode(t *testing.T) {
 	ent := Entry{key: []byte("k1"), val: []byte("xxx")}
-	data := []byte{2, 0, 0, 0, 3, 0, 0, 0, 'k', '1', 'x', 'x', 'x'}
+	// 2文字 + 3文字 + false + k1 + xxx
+	data := []byte{2, 0, 0, 0, 3, 0, 0, 0, 0, 'k', '1', 'x', 'x', 'x'}
 
 	assert.Equal(t, data, ent.Encode())
 
 	decoded := Entry{}
 	err := decoded.Decode(bytes.NewBuffer(data))
+	assert.Nil(t, err)
+	assert.Equal(t, ent, decoded)
+
+	ent = Entry{key: []byte("k1"), deleted: true}
+	// 2文字 + 0文字 + true + k1
+	data = []byte{2, 0, 0, 0, 0, 0, 0, 0, 1, 'k', '1'}
+
+	assert.Equal(t, data, ent.Encode())
+
+	decoded = Entry{}
+	err = decoded.Decode(bytes.NewBuffer(data))
 	assert.Nil(t, err)
 	assert.Equal(t, ent, decoded)
 }
